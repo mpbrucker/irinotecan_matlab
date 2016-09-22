0cbbb2ff4fa641d2c9ed6775ccef034ed78708f8
William Derksen <wderksen@olin.edu>
2016-09-22 17:05:36 -0400

    Matlab Initial Model

---
+++ isInteger.m
@@ -0,0 +1,15 @@
+function res = isInteger(x)
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+
+if round(x) == x;
+    res = 1;
+else
+    res = 0;
+end
+
+
+
+
+end
+
---
+++ runModel.m
@@ -0,0 +1,26 @@
+clf;
+hold on;
+stocks = [.1 0 0 0 0 0]; % Chemical compounds concentraion in order of CPT_11, SN_38, SN_38G, APC, NPC, and Excreted Material
+%CPT_11, SN_38, SN_38G, APC, NPC
+%CPT has to be non zero
+
+variables = [10 3 35 .014583333]; %Enzyme concentration and excretion rate in order of Esterase, CYP3A, UGT, Excr. rate
+%Esterase, CYP3A, UGT, Excr. rate
+
+doseCPT = 360; %mg
+
+simlen = 300; %hours
+
+for i = 1:simlen
+    if isInteger((i-1)/24)
+        stocks = Update(doseCPT, variables, stocks)
+    else
+        stocks = Update(0, variables, stocks)
+    end
+    plot(i, stocks(1), 'ko', 'MarkerSize', 6)
+    plot(i, stocks(2), 'ro', 'MarkerSize', 6)
+    plot(i, stocks(3), 'bo', 'MarkerSize', 6)
+    plot(i, stocks(4), 'go', 'MarkerSize', 6)
+    plot(i, stocks(5), 'mo', 'MarkerSize', 6)
+    %plot(i, stocks(6), 'co', 'MarkerSize', 6)
+end
